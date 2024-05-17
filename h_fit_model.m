674f061fc1d4b2ac685b122b6afb5bec94abdca0
Hamid Turker <hamidturker@gmail.com>
2024-05-16 21:55:28 -0400

    Add files via upload

--- h_fit_model.m
+++ h_fit_model.m
@@ -1,5 +1,8 @@
 function [testFit,trainFit,param_mean] = h_fit_model(A,dt,spiketrain,filter,modelType,numFolds,ln_params)
 
+%% @ HBT - 2024 May 16
+%    Adjusted the original code to allow for flexible number of parameters
+
 %% Description
 % This code will section the data into 10 different portions. Each portion
 % is drawn from across the entire recording session. It will then
